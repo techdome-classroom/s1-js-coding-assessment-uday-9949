7547122fe1c9a98759b6d7a361979fb963abd30d
PULI UDAY KIRAN <119495444+uday-9949@users.noreply.github.com>
2024-11-11 13:46:13 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,8 +1,6 @@
-import re
+const decodeTheRing = function (s, p) {
+    const doubleS = s + s;
+    return doubleS.includes(p);
+};
 
-def decode_message(s: str, p: str) -> bool:
-    # Replace ? with . (single character wildcard) and * with .* (zero or more characters)
-    p = p.replace("?", ".").replace("*", ".*")
-    
-    # Match the entire pattern with the message
-    return bool(re.fullmatch(p, s))
+module.exports = decodeTheRing;
